0583c0f62e579c4f3a9aef8a384a1f51239d2f7f
VedankWakalkar <131476793+VedankWakalkar@users.noreply.github.com>
2025-01-01 12:40:37 +0530

    Added Sidebar Component and Dashboard Pages

---
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -0,0 +1,37 @@
+import { SideBarItem } from "../../components/SideBarItem"
+
+export default function Layout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }): JSX.Element {
+    return(<div className="flex">
+        <div className="w-72 border-r border-slate-300 min-h-screen mr-4 pt-28">
+            <SideBarItem href={"/dashboard"} icon={<HomeIcon/>} title={"Home"}/>
+            <SideBarItem href={"/transaction"} icon={<TransactionsIcon/>} title={"Transaction"}/>
+            <SideBarItem href={"/transfer"} icon={<TransferIcon/>} title={"Transfer"}/>
+        </div>
+        <div>
+            {children}
+        </div>
+    </div>
+    )
+}
+
+function HomeIcon(){
+   return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+    </svg>
+}
+
+function TransactionsIcon(){
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
+  </svg>  
+}
+
+function TransferIcon(){
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M15 8.25H9m6 3H9m3 6-3-3h1.5a3 3 0 1 0 0-6M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+  </svg>  
+}
---
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function(){
+    return <div>
+        hi there from the transfer page
+    </div>
+}
